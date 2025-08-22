bb98fa38a279858cbd61aaba84cc7d261d25a83a
MertUras <77569047+MertUras@users.noreply.github.com>
2025-08-22 13:48:19 +0300

    minor changes error fixes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4b8e9cc57691205f"; }
+/******/ 		__webpack_require__.h = function() { return "b99775572f1f2ab1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
